a6ab62445b22db6f9efc8c1cb4e93d465e8fd0bf
blny5f <168887180+blny5f@users.noreply.github.com>
2024-10-20 13:19:13 -0500

    prereq table

--- Course_Scheduling_Database.py
+++ Course_Scheduling_Database.py
@@ -6,20 +6,27 @@ def table_init():
     file = sqlite3.connect("Course_Info.db")
     cur = file.cursor()
 
-    # initialize the entity 'Course' and each of its attributes
+    # initialize the entity 'COURSE' and each of its attributes
     # list of attributes is not finalized, will be updated as needed
+    # NUMBER refers to, for example, 1500 in Comp Sci 1500
+    # DEPARTMENT is the official shortened name of the dept, e.g. CHEM ENG, ELEC ENG, etc., it should always fit in a varchar(8)
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS Course(
-            COURSE_TITLE varchar(63),
-            COURSE_CODE varchar(15), 
-            COURSE_DESCRIPTION varchar(1023),
-            CREDIT_HOURS float(1),
-            PREREQUISITE_CODES varchar(15),
+        CREATE TABLE IF NOT EXISTS COURSE(
+            TITLE varchar(63) NOT NULL PRIMARY KEY,
+            DEPARTMENT varchar(8),
+            NUMBER int, 
+            DESCRIPTION varchar(1023),
+            NUM_CREDITS float(1)
             
-            
+        )
+    """)
 
-
-
+    # Recursive 'PREREQUISITE' relation, each row links a course to one of its prerequisites
+    # if a course has multiple prerequisites, it will appear in multiple rows
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS PREREQUISITE(
+            TITLE varchar(63),
+            PREREQUISITE_TITLE varchar(63)
 
         )
     """)
