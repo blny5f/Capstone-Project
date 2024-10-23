15b527cdd1ecbb24265903119c285c840962dc2b
user <blny5f@umsystem.edu>
2024-10-22 21:02:01 -0500

    bugfix

--- database.py
+++ database.py
@@ -22,7 +22,7 @@ def database_init():
             numCredits FLOAT(1),
             Available BOOLEAN
         )
-    """)
+    """);
 
     # Recursive 'REQUIRES' relation, each row links a course to one of its prerequisites
     # if a course has multiple prerequisites, it will appear in multiple rows
@@ -32,24 +32,24 @@ def database_init():
             prereqTitle VARCHAR(63) NOT NULL,
             gradeReq VARCHAR(1)
         )
-    """)
+    """);
 
     # 'ENROLLED' relation, each row contains a studentID and the title of one of the courses in which they are enrolled
     cur.execute("""
         CREATE TABLE IF NOT EXISTS ENROLLED(
             studentID VARCHAR(8) NOT NULL,
-            courseTitle VARCHAR(63) NOT NULL,
+            courseTitle VARCHAR(63) NOT NULL
         )
-    """)
+    """);
 
     # 'COMPLETED' relation, each row contains a studentID, the title of a course which they have completed, and their final grade
     cur.execute("""
         CREATE TABLE IF NOT EXISTS COMPLETED(
             studentID VARCHAR(8) NOT NULL,
             courseTitle VARCHAR(63) NOT NULL,
-            grade VARCHAR(1) NOT NULL,
+            grade VARCHAR(1) NOT NULL
         )
-    """)
+    """);
 
     # 'USER_PROFILE' entity, used to store information specific to each student
     # user will log in using email and password
@@ -60,9 +60,9 @@ def database_init():
             lastName VARCHAR(24),
             Password VARCHAR(24) NOT NULL,
             Email VARCHAR(31) NOT NULL,
-            Standing VARCHAR(10),
+            Standing VARCHAR(10)
         )
-    """)
+    """);
 
     # 'ATTENDS' relation, each row contains a studentID and the classID of a class they attend
     cur.execute("""
@@ -70,7 +70,7 @@ def database_init():
             studentID VARCHAR(8) NOT NULL,
             classID VARCHAR(8) NOT NULL
         )
-    """)
+    """);
 
     # 'CLASS' entity, stores information about the classes being held for each course
     # classType attribute distinguishes lecture/lab/recitation
@@ -87,7 +87,7 @@ def database_init():
             Location TEXT,
             timeHeld TIME
         )
-    """)
+    """);
 
     # 'OFFERS' relation, links the courses to each of their respective classes
     cur.execute("""
@@ -95,8 +95,22 @@ def database_init():
             courseTitle VARCHAR(63) NOT NULL,
             classID VARCHAR(8) NOT NULL
         )
-    """)
+    """);
+
+    # TEMPORARY TEST DATA
+    cur.execute("""
+        REPLACE INTO COURSE(Title, Department, Number, Description, numCredits, Available) VALUES
+            ('Python', 'Comp Sci', 1500, 'We teach python here', 3, 1),
+            ('C++', 'Comp Sci', 1575, 'We teach c++ here', 3, 1),
+            ('Boolean Algebra', 'Comp Sci', 2200, 'We teach boolean algebra here', 3, 1),
+            ('Java', 'Comp Sci', 2400, 'We teach java here', 3, 0)
+    """);
+
+    cur.execute("""
+        REPLACE INTO USER_PROFILE(studentID, firstName, lastName, Password, Email, Standing) VALUES
+            ('1', 'James', 'Sullivan', 'password123', 'jss4kh@mst.edu', 'Senior'),
+            ('2', 'Mike', 'Wazowski', 'pass12345', 'msw123@mst.edu', 'Senior')
+    """);
 
     file.commit()
     file.close()
-
