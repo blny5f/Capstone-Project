3148ce0bfc1327bca8170ea14f45f2dd8df9d81c
user <blny5f@umsystem.edu>
2024-11-04 10:44:16 -0600

    login function

--- functions.py
+++ functions.py
@@ -1,20 +1,24 @@
 # functions.py
 import sqlite3 as sq
 
+# search function for course list
 def search():
   file = sq.connect("Course_Info.db")
   cur = file.cursor()
 
+  # take string to search as input and select matches
   search_term = input("Search: ")
+  print("\nSearching...")
   cur.execute("""SELECT Title, Department, Number, numCredits, Available
     FROM Course
     WHERE Title LIKE ?
     """, ('%' + search_term + '%',))
 
-  print('\nSearching...')
+  # fetch results and print the number of results found
   results = cur.fetchall()
   print(f'\n{len(results)} Results\n')
 
+  # basic UI to format results cleanly
   col1 = 'TITLE'
   col2 = 'DEPARTMENT'
   col3 = 'NUMBER'
@@ -23,9 +27,13 @@ def search():
   col_gap = 30
   num_divs = 142
 
+  # print headers of each column
   print(f'{col1:<{col_gap}} {col2:<{col_gap}} {col3:<{col_gap}} {col4:<{col_gap}} {col5:<{col_gap}}')
   print('-' * num_divs)
 
+
+  # recursively print each result
+  # convert 'Available' boolean in course[4] to a readable format
   for course in results:
     if(course[4] == 1):
       avail = 'AVAILABLE'
@@ -37,13 +45,15 @@ def search():
   print('-' * num_divs)
 
 
+# login function to call as website is opened
 def login():
+  # take email and password as input
   print('-' * 142)
   print("Login with your university Email and password.")
-
   email = input("Email: ")
   password = input("Password: ")
 
+  # connect to database and find a match for the given login information
   print('-' * 142)
   file = sq.connect("Course_Info.db")
   cur = file.cursor()
@@ -53,6 +63,8 @@ def login():
     AND Password LIKE ?""", (email, password)
   )
 
+  # if no results from fetchone(), invalid login information, try again
+  # otherwise, greet user with their first name
   profile = cur.fetchone()
   if profile is None:
     print("Invalid Email or password.")
@@ -61,7 +73,9 @@ def login():
     print(f'Welcome, {profile[2]}!')
 
 
+# menu function, called after logging in
 def menu():
+  # display options
   print("\n1: Search the course list\n2: option 2\n3: Exit the program")
 
   # loop until user enters valid number
@@ -69,10 +83,10 @@ def menu():
     try:
       option = int(input("\nEnter a number to select an option: "))
       break
-
     except ValueError:
       print("Please enter a valid number.")
 
+  # match inputted integer with corresponding option function
   match option:
     case 1:
       search()
